a5e493ecb5902cb4aaa4f218b2fdc9d3110e2102
vanhau13207-debug <vanhau13207@gmail.com>
2025-11-24 20:55:46 +0700

    Add video upload function using YouTube API

--- uploader.py
+++ uploader.py
@@ -1,1 +1,23 @@
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+import os
 
+def upload(video, title, desc, schedule_time):
+    yt = build("youtube", "v3", developerKey=os.getenv("YT_API"))
+
+    request = yt.videos().insert(
+        part="snippet,status",
+        body={
+            "snippet": {
+                "title": title,
+                "description": desc,
+                "categoryId": "24"
+            },
+            "status": {
+                "privacyStatus": "private",
+                "publishAt": schedule_time
+            }
+        },
+        media_body=MediaFileUpload(video)
+    )
+    request.execute()
