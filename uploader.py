fa0bacb171c2d56ed9684045f744313adcba5469
vanhau13207-debug <vanhau13207@gmail.com>
2025-11-28 16:28:22 +0700

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -1,23 +1,35 @@
+from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-import os
+import datetime, os
 
-def upload(video, title, desc, schedule_time):
-    yt = build("youtube", "v3", developerKey=os.getenv("YT_API"))
+def upload(video_path, title, description, schedule_time):
+    creds = Credentials(
+        None,
+        refresh_token=os.getenv("YT_REFRESH_TOKEN"),
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=os.getenv("YT_CLIENT_ID"),
+        client_secret=os.getenv("YT_CLIENT_SECRET"),
+        scopes=["https://www.googleapis.com/auth/youtube.upload"],
+    )
+
+    yt = build("youtube", "v3", credentials=creds)
 
     request = yt.videos().insert(
         part="snippet,status",
         body={
             "snippet": {
                 "title": title,
-                "description": desc,
-                "categoryId": "24"
+                "description": description,
+                "categoryId": "24",
             },
             "status": {
                 "privacyStatus": "private",
                 "publishAt": schedule_time
             }
         },
-        media_body=MediaFileUpload(video)
+        media_body=MediaFileUpload(video_path)
     )
-    request.execute()
+
+    response = request.execute()
+    print("Uploaded:", response)
